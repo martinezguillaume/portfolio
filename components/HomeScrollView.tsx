7e2a1ff321754c7badae509fe0d6f2b8f8ae7560
guizmo <martinezguillaume@users.noreply.github.com>
2022-02-21 17:18:48 +0100

    Update HomeScrollView.tsx

--- components/HomeScrollView.tsx
+++ components/HomeScrollView.tsx
@@ -146,6 +146,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center',
+    overflow: 'hidden',
   },
   coverWrapper: {
     position: 'absolute',
