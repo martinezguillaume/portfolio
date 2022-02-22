447a62eade3fc8cec492a8d2c9bf873cddc049e6
guizmo <martinezguillaume@users.noreply.github.com>
2022-02-22 23:50:22 +0100

    Add profile descriptions

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { t } from 'i18n-js'
-import { Box, Fab, Icon, Text, useColorMode } from 'native-base'
-import { FC } from 'react'
+import { Box, Fab, Icon, Row, Text, useColorMode } from 'native-base'
+import { FC, useMemo } from 'react'
 import { StyleSheet } from 'react-native'
 import { Feather } from '@native-base/icons'
 
@@ -12,8 +12,32 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Home'>
 
 export const HomeScreen: FC<Props> = () => {
   const { toggleColorMode } = useColorMode()
+
+  const data = useMemo<{ description?: string; icon: string; link?: boolean }[]>(
+    () => [
+      {
+        icon: 'map-pin',
+        description: t('home.locationDescription'),
+      },
+      {
+        icon: 'gift',
+        description: t('home.birthdayDescription'),
+      },
+      {
+        icon: 'link',
+        link: true,
+        description: 'martinezguillaume.github.com',
+      },
+      {
+        icon: 'calendar',
+        description: t('home.developerDescription'),
+      },
+    ],
+    []
+  )
+
   return (
-    <>
+    <Box height="100%" maxW={500} alignSelf="center">
       <HomeScrollView contentContainerStyle={styles.scrollViewContent}>
         <Text fontSize={16} fontWeight={800}>
           Guillaume Martinez
@@ -22,20 +46,17 @@ export const HomeScreen: FC<Props> = () => {
 
         <Text mt={4}>{t('home.description')}</Text>
 
-        <Box flexDirection="row" mt={4} alignItems="center">
-          <Text fontSize={12} ml={1} color="blue.500" width={100} numberOfLines={1}>
-            <Icon color="muted.500" size="4" as={Feather} name="link" />{' '}
-            martinezguillaume.github.com
-          </Text>
-          <Text fontSize={12} ml={2} color="muted.500" numberOfLines={1}>
-            <Icon color="muted.500" size="4" as={Feather} name="calendar" /> {t('home.date_title')}
-          </Text>
-        </Box>
+        <Row flexDirection="row" flexWrap="wrap" mt={2} space={2}>
+          {data.map((item) => (
+            <Text key={item.icon} fontSize={12} ml={1} mt={2} color="muted.500">
+              <Icon color="muted.500" size="4" as={Feather} name={item.icon} /> {item.description}
+            </Text>
+          ))}
+        </Row>
         <Box height={3000} />
       </HomeScrollView>
-
       <Fab onPress={toggleColorMode} />
-    </>
+    </Box>
   )
 }
 
