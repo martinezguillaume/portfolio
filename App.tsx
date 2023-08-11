542f90ce195aa394273ecfb5132a124f01104692
guizmo <martinezguillaume@users.noreply.github.com>
2023-08-11 14:30:57 -0600

    Update to Expo SDK 49

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { INativebaseConfig, NativeBaseProvider, StorageManager } from 'native-base'
+import { StyleSheet, View } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import i18n from 'i18n-js'
 import * as Localization from 'expo-localization'
 import * as Font from 'expo-font'
 import { Feather, MaterialCommunityIcons } from '@native-base/icons'
-import { useCallback, useState } from 'react'
-import AppLoading from 'expo-app-loading'
+import { useCallback, useEffect, useState } from 'react'
+import * as SplashScreen from 'expo-splash-screen'
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 
@@ -51,33 +52,60 @@ const config: INativebaseConfig = {
   theme,
 }
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync()
+
 export default function App() {
-  const [isReady, setIsReady] = useState(false)
+  const [appIsReady, setAppIsReady] = useState(false)
 
-  const loadAssets = useCallback(async () => {
-    // const cacheImages = Object.values(IMAGES).map((image) =>
-    //   Asset.fromModule(image).downloadAsync()
-    // )
-    const cacheFonts = [Feather.font, MaterialCommunityIcons.font].map((font) =>
-      Font.loadAsync(font)
-    )
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // const cacheImages = Object.values(IMAGES).map((image) =>
+        //   Asset.fromModule(image).downloadAsync()
+        // )
+        const cacheFonts = [Feather.font, MaterialCommunityIcons.font].map((font) =>
+          Font.loadAsync(font)
+        )
 
-    await Promise.all([...cacheFonts])
+        await Promise.all([...cacheFonts])
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
   }, [])
 
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadAssets}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    )
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
   }
 
   return (
-    <NativeBaseProvider colorModeManager={colorModeManager} config={config}>
-      <Navigation />
-    </NativeBaseProvider>
+    <View style={styles.flex} onLayout={onLayoutRootView}>
+      <NativeBaseProvider colorModeManager={colorModeManager} config={config}>
+        <Navigation />
+      </NativeBaseProvider>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+})
