3c17bbc187295e24a01fc6a302eae7d0c36427e2
guizmo <martinezguillaume@users.noreply.github.com>
2024-02-12 19:37:54 +0100

    Refacto Icon

--- components/base/Icon.tsx
+++ components/base/Icon.tsx
@@ -11,16 +11,11 @@ export type IconProps =
     } & ComponentProps<typeof MaterialCommunityIcons>)
 
 export const Icon = ({className, ...props}: IconProps) => {
-  const defaultClassName = 'text-primary text-base'
+  const newClassName = `text-primary text-base ${className}`
 
   if (props.type === 'material-community-icons') {
-    return (
-      <MaterialCommunityIcons
-        className={`${defaultClassName} ${className}`}
-        {...props}
-      />
-    )
+    return <MaterialCommunityIcons className={newClassName} {...props} />
   }
 
-  return <Feather className={`${defaultClassName} ${className}`} {...props} />
+  return <Feather className={newClassName} {...props} />
 }
